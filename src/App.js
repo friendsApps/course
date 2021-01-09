64b50aad86551138cf6fcdd8937d82d334cd19ec
Andre Souza <ajjunior33@gmail.com>
2021-01-09 18:47:35 -0300

    Finish project.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { FiSettings } from "react-icons/fi";
+import { BiPencil, BiWebcam,BiChat, BiBox, BiBraille} from "react-icons/bi";
 // Components
-import Central from './components/Central';
+import Central from "./components/Central";
 import LateralOne from "./components/LateralOne";
 
 // Img Files
-import profile from './assets/images/profile.png';
+import profile from "./assets/images/profile.png";
 function App() {
   return (
     <div className="container">
       <LateralOne />
-      <Central/>
+      <Central />
 
       <section id="lateral-2">
         <div className="profile">
@@ -22,27 +23,55 @@ function App() {
         <div className="progress">
           <h3>Progress</h3>
 
-          <div className="item-progress">
-            <div>
-              <FiSettings />
-              <div>
-                <h3>UI/UX Design</h3>
-                <small>Advanced</small>
-              </div>
+          <div className="itemtask">
+            <div className="icon" style={{ backgroundColor: "#EEF2FD" }}>
+              <BiPencil size={20} color="#5E81F4" />
             </div>
-            <div>
-              <FiSettings />
-              <div>
-                <h3>UI/UX Design</h3>
-                <small>Advanced</small>
-              </div>
+            <div className="data">
+              <h4>UI/UX Design</h4>
+              <span>Advanced</span>
             </div>
-            <div>
-              <FiSettings />
-              <div>
-                <h3>UI/UX Design</h3>
-                <small>Advanced</small>
-              </div>
+          </div>
+
+          <div className="itemtask">
+            <div className="icon" style={{ backgroundColor: "#D5F5FB" }}>
+              <BiWebcam size={20} color="#0CC3E7" />
+            </div>
+            <div className="data">
+              <h4>Photography</h4>
+              <span>Intermediate</span>
+            </div>
+          </div>
+
+          <div className="itemtask">
+            <div className="icon" style={{ backgroundColor: "#FFEFD6" }}>
+              <BiBraille size={20} color="#FFAE33" />
+            </div>
+            <div className="data">
+              <h4>Animation</h4>
+              <span>Advanced</span>
+            </div>
+          </div>
+
+
+          <h3>Upcoming Task</h3>
+
+          <div className="itemtask">
+            <div className="icon" style={{ backgroundColor: "#EEF2FD" }}>
+              <BiChat size={20} color="#5E81F4" />
+            </div>
+            <div className="data">
+              <h4>UI/UX - Discussion</h4>
+              <span>27 Oct 2020, Tuesday</span>
+            </div>
+          </div>
+          <div className="itemtask">
+            <div className="icon" style={{ backgroundColor: "#FFEFD6" }}>
+              <BiBox size={20} color="#FFAE33" />
+            </div>
+            <div className="data">
+              <h4>Animation - 3D Animation</h4>
+              <span>27 Oct 2020, Tuesday</span>
             </div>
           </div>
         </div>
--- src/components/LateralOne.js
+++ src/components/LateralOne.js
@@ -1,55 +1,69 @@
-import {FiLayout, FiFolder,FiGrid,FiUser,FiMessageSquare,FiSettings } from 'react-icons/fi';
-import logo from '../assets/images/logo.svg';
-import Frame from '../assets/images/Frame.png';
-import '../style/components/lateralOne.css';
-function LateralOne()
-{return (
+import {
+  FiLayout,
+  FiFolder,
+  FiGrid,
+  FiUser,
+  FiMessageSquare,
+  FiSettings,
+} from "react-icons/fi";
+import logo from "../assets/images/logo.svg";
+import Frame from "../assets/images/Frame.png";
+import "../style/components/lateralOne.css";
+function LateralOne() {
+  return (
     <section className="lateral-1">
-    <div>
-      <img src={logo} id="logo" alt="Logo" />
-      <button className="button button_primary "> Join a course </button>
-    </div>
-    <nav>
-      
-      <ul>
-        <li>
-          <FiLayout/>
-          <span>Dashboard</span>
-        </li>
-        <li className="active">
-          <FiGrid/>
-          <span>
-          All Courses</span>
-        </li>
-        <li>
-          <FiFolder/>
-          <span>Resources</span>
-        </li>
-        <li>
-          <FiUser/>
-          <span>Friends</span>
-        </li>
-        <li>
-          <FiMessageSquare/>
-          <span>Chats</span>
-        </li>
-        <li>
-          <FiSettings/>
-          <span>Settings</span>
-        </li>
-      </ul>
+      <div>
+        <img src={logo} id="logo" alt="Logo" />
+        <button className="button button_primary "> Join a course </button>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">
+              <FiLayout />
+              <span>Dashboard</span>
+            </a>
+          </li>
+          <li className="active">
+            <a href="#">
+              <FiGrid />
+              <span>All Courses</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <FiFolder />
+              <span>Resources</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <FiUser />
+              <span>Friends</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <FiMessageSquare />
+              <span>Chats</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <FiSettings />
+              <span>Settings</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
 
-    </nav>
+      <footer>
+        <img src={Frame} alt="file" />
+        <strong>Upgrade to for more resources</strong>
+        <button className="button button_primary">Upgrade</button>
+      </footer>
+    </section>
+  );
+}
 
-    <footer>
-      <img src={Frame} alt="file" />
-      <strong>
-        Upgrade to for more resources
-      </strong>
-      <button className="button button_primary">Upgrade</button>
-    </footer>
-
-  </section>
-)}
-
-export default LateralOne 
+export default LateralOne;
